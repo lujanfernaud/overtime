900f51cdbb37fd2b2492bcedb2ea6792237e78fc
lujanfernaud <lujanfernaud@gmail.com>
2018-10-27 17:37:53 +0100

    Add StaticsController#index and root_path

---
+++ spec/features/static_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Static Pages", type: :feature do
+  describe "navigate" do
+    describe "homepage" do
+      it "can be reached successfully" do
+        visit root_path
+
+        expect(page.status_code).to eq(200)
+      end
+    end
+  end
+end
