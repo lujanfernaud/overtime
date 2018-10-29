21e50c5a01dc47253378ab653b3d4b586d78754a
lujanfernaud <lujanfernaud@gmail.com>
2018-10-29 13:43:27 +0000

    Use 'build_stubbed' instead of 'build'

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "creation" do
     it "can be created" do
-      post = build(:post)
+      post = build_stubbed(:post)
 
       expect(post).to be_valid
     end
 
     it "cannot be created without date" do
-      post = build(:post, date: nil)
+      post = build_stubbed(:post, date: nil)
 
       expect(post).not_to be_valid
     end
 
     it "cannot be created without rationale" do
-      post = build(:post, rationale: nil)
+      post = build_stubbed(:post, rationale: nil)
 
       expect(post).not_to be_valid
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "creation" do
     it "can be created" do
-      user = build(:user)
+      user = build_stubbed(:user)
 
       expect(user).to be_valid
     end
 
     it "cannot be created without first_name and last_name" do
-      user = build(:user, first_name: nil, last_name: nil)
+      user = build_stubbed(:user, first_name: nil, last_name: nil)
 
       expect(user).to_not be_valid
     end
@@ -17,13 +17,13 @@ RSpec.describe User, type: :model do
 
   describe "#full_name_with_comma" do
     it "shows full name with last name first and comma separator" do
-      user = build(:user, first_name: "Yao", last_name: "Dao")
+      user = build_stubbed(:user, first_name: "Yao", last_name: "Dao")
 
       expect(user.full_name_with_comma).to eq("Dao, Yao")
     end
 
     it "shows full name capitalized" do
-      user = build(:user, first_name: "yao", last_name: "dao")
+      user = build_stubbed(:user, first_name: "yao", last_name: "dao")
 
       expect(user.full_name_with_comma).to eq("Dao, Yao")
     end
