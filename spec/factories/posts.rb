d2d1219b934faab99216f0813d87ccd6ee126504
lujanfernaud <lujanfernaud@gmail.com>
2018-10-30 20:22:01 +0000

    Add status to Post using ActiveRecord::Enum

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,5 +3,14 @@ FactoryBot.define do
     user
     date      { Time.zone.now }
     rationale { "Test rationale." }
+    status    { "submitted" }
+
+    trait :approved do
+      status { "approved" }
+    end
+
+    trait :rejected do
+      status { "rejected" }
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -20,4 +20,26 @@ RSpec.describe Post, type: :model do
       expect(post).not_to be_valid
     end
   end
+
+  describe "#status" do
+    it "has a default value of 'submitted'" do
+      post = build_stubbed(:post)
+
+      expect(post.status).to eq("submitted")
+    end
+
+    it "can be set to 'approved'" do
+      post = create(:post)
+      post.approved!
+
+      expect(post.status).to eq("approved")
+    end
+
+    it "can be set to 'rejected'" do
+      post = create(:post)
+      post.rejected!
+
+      expect(post.status).to eq("rejected")
+    end
+  end
 end
